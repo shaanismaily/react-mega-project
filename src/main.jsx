b7e712a5de0773409b1ed1a02d44ecfc5c2e7b38
Muhammad Shaan <shaanrazaismaily@gmail.com>
2026-04-16 18:32:52 +0530

    Set-up store and create header and footer components

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,14 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { StrictMode } from 'react'
+import { Provider } from 'react-redux'
+import store from './store/store.js'
 
 createRoot(document.getElementById('root')).render(
-    <App />
+    <StrictMode>
+    <Provider store={store}>
+        <App />
+    </Provider>
+    </StrictMode>
 )
